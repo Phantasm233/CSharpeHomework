343b4a2a8d2e288dab3852c6cd0f21e90ffb3a3f
ZZT <1491673014@qq.com>
2019-02-24 17:30:01 +0800

    c#第一次作业

---
+++ homework1/homework1.1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入两个数字:");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("两个数字之积为{0}", num1*num2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ homework1/homework1.2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(this.textBox1.Text);
+            int num2 = int.Parse(this.textBox2.Text);
+            this.label1.Text = (num1 * num2).ToString();
+        }
+    }
+}
