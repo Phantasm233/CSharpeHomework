09aeafa1e5b752c1bc1640559cba8a0fc4473bfc
ZZT <1491673014@qq.com>
2019-03-10 14:23:14 +0800

    C#第三次作业

---
+++ homework3/homework3.1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Shape shape;
+                shape = Factory.getShape("circle");
+                shape.Draw();
+                shape.Erase();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("绘制图形无法识别！");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    //公共接口
+    interface Shape
+    {
+        void Draw();
+        void Erase();
+    }
+
+    //实现公共接口的具体图形类
+    class Circle : Shape
+    {
+        public Circle()
+        {
+            Console.WriteLine("创建圆！");
+        }
+        public void Draw()
+        {
+            Console.WriteLine("画圆！");
+        }
+        public void Erase()
+        {
+            Console.WriteLine("清除圆！");
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public Triangle()
+        {
+            Console.WriteLine("创建三角形！");
+        }
+        public void Draw()
+        {
+            Console.WriteLine("画三角形！");
+        }
+        public void Erase()
+        {
+            Console.WriteLine("清除三角形！");
+        }
+    }
+
+    class Square : Shape
+    {
+        public Square()
+        {
+            Console.WriteLine("创建正方形！");
+        }
+        public void Draw()
+        {
+            Console.WriteLine("画正方形！");
+        }
+        public void Erase()
+        {
+            Console.WriteLine("清除正方形！");
+        }
+    }
+
+    //工厂类
+    class Factory
+    {
+        public static Shape getShape(string type)
+        {
+            Shape shape = null;
+            if(type.Equals("circle"))
+            {
+                shape = new Circle();
+                Console.WriteLine("初始化圆！");
+            }
+            else if(type.Equals("triangle"))
+            {
+                shape = new Triangle();
+                Console.WriteLine("初始化三角形！");
+            }
+            else if(type.Equals("square"))
+            {
+                shape = new Square();
+                Console.WriteLine("初始化正方形！");
+            }
+            else
+            {
+                Console.WriteLine("UnsupportedShapeException!");
+            }
+            return shape;
+        }
+    }
+}
---
+++ homework3/homework3.2.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ReaderFactory factory;  //抽象工厂
+                PhotoReader reader;  //抽象产品
+                factory = new GIF_ReaderFactory();
+                reader = factory.Createreader();
+                reader.Readphoto();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("无效的图片读取器！");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    //图片读取器：抽象产品
+    interface PhotoReader
+    {
+        void Readphoto();
+    }
+
+    //图片读取器：具体产品
+    class GIF_Reader:PhotoReader
+    {
+        public void Readphoto()
+        {
+            Console.WriteLine("读取GIF图片！");
+        }
+    }
+    class JPG_Reader:PhotoReader
+    {
+        public void Readphoto()
+        {
+            Console.WriteLine("读取JPG图片！");
+        }
+    }
+    class PNG_Reader : PhotoReader
+    {
+        public void Readphoto()
+        {
+            Console.WriteLine("读取PNG图片！");
+        }
+    }
+
+    //图片读取器工厂接口：抽象工厂
+    interface ReaderFactory
+    {
+        PhotoReader Createreader();
+    }
+
+    //图片读取器工厂类：具体工厂
+    class GIF_ReaderFactory:ReaderFactory
+    {
+        public PhotoReader Createreader()
+        {
+            PhotoReader reader = new GIF_Reader();
+            Console.WriteLine("创建GIF图片读取器！");
+            return reader;
+        }
+    }
+    class JPG_ReaderFactory : ReaderFactory
+    {
+        public PhotoReader Createreader()
+        {
+            PhotoReader reader = new JPG_Reader();
+            Console.WriteLine("创建JPG图片读取器！");
+            return reader;
+        }
+    }
+    class PNG_ReaderFactory : ReaderFactory
+    {
+        public PhotoReader Createreader()
+        {
+            PhotoReader reader = new PNG_Reader();
+            Console.WriteLine("创建PNG图片读取器！");
+            return reader;
+        }
+    }
+}
