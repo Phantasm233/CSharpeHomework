9c75d850c505ef31c56eaa362f47c9bd3d20e5f7
ZZT <1491673014@qq.com>
2019-04-14 14:59:35 +0800

    C#第七次作业

---
+++ homework7/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS7
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        double th1;
+        double th2;
+        double per1;
+        double per2;
+        double k;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree(10, 200, 310, 100, -Math.PI / 2);
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            th1 = double.Parse(comboBox1.Text)*Math.PI/180;
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            per1 = double.Parse(comboBox3.Text);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            th2 = double.Parse(comboBox2.Text) * Math.PI / 180;
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            per2 = double.Parse(comboBox4.Text);
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            k = double.Parse(comboBox5.Text);
+        }
+
+        void drawCayleyTree(int n,double x0,double y0,double leng,double th)
+        {
+            if (n == 0) return;
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            double x2 = x0 + leng * k * Math.Cos(th);
+            double y2 = y0 + leng * k * Math.Sin(th);
+
+            try
+            {
+                drawLine(x0, y0, x1, y1);
+                drawLine(x0, y0, x1, y1);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error occured!");
+            }
+
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x2, y2, per2 * leng, th - th2);
+        }
+
+        void drawLine(double x0,double y0,double x,double y)
+        {
+            graphics.DrawLine(
+                Pens.Blue,
+                (int)x0, (int)y0, (int)x, (int)y);
+        }
+    }
+}
---
+++ homework7/OrderServiceTest1.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CS5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS5.Test1
+{
+    [TestClass()]
+    public class OrderServiceTest1
+    {
+        [TestMethod()]
+        public void InsertTest1()
+        {
+            Customer jack = new Customer(1, "Jack");
+            Goods pen = new Goods(1, "Pen", 150);
+            Goods pencil = new Goods(2, "Pencil", 5.99);
+            Goods notebook = new Goods(3, "Notebook", 15.59);
+            OrderDetails orderDetails1 = new OrderDetails(1, pen, 15);
+            OrderDetails orderDetails2 = new OrderDetails(2, pencil, 60);
+            OrderDetails orderDetails3 = new OrderDetails(3, notebook, 50);
+            Order order = new Order(1, jack);
+            order.AddDetails(orderDetails1);
+            order.AddDetails(orderDetails2);
+            order.AddDetails(orderDetails3);
+            OrderService os = new OrderService();
+            os.Insert(order);
+            Assert.IsNotNull(os.orderList);
+        }
+
+        [TestMethod()]
+        public void GetByIdTest1()
+        {
+            Customer jack = new Customer(1, "Jack");
+            Goods pen = new Goods(1, "Pen", 150);
+            Goods pencil = new Goods(2, "Pencil", 5.99);
+            Goods notebook = new Goods(3, "Notebook", 15.59);
+            OrderDetails orderDetails1 = new OrderDetails(1, pen, 15);
+            OrderDetails orderDetails2 = new OrderDetails(2, pencil, 60);
+            OrderDetails orderDetails3 = new OrderDetails(3, notebook, 50);
+            Order order = new Order(1, jack);
+            order.AddDetails(orderDetails1);
+            order.AddDetails(orderDetails2);
+            order.AddDetails(orderDetails3);
+            OrderService os = new OrderService();
+            os.Insert(order);
+            Order od = os.GetById(order.orderId);
+            Assert.IsNotNull(od);
+        }
+
+        [TestMethod()]
+        public void FindByCustomerNameTest1()
+        {
+            Customer jack = new Customer(1, "Jack");
+            Goods pen = new Goods(1, "Pen", 150);
+            Goods pencil = new Goods(2, "Pencil", 5.99);
+            Goods notebook = new Goods(3, "Notebook", 15.59);
+            OrderDetails orderDetails1 = new OrderDetails(1, pen, 15);
+            OrderDetails orderDetails2 = new OrderDetails(2, pencil, 60);
+            OrderDetails orderDetails3 = new OrderDetails(3, notebook, 50);
+            Order order = new Order(1, jack);
+            order.AddDetails(orderDetails1);
+            order.AddDetails(orderDetails2);
+            order.AddDetails(orderDetails3);
+            OrderService os = new OrderService();
+            os.Insert(order);
+            List<Order> odList = os.FindByCustomerName(order.customer.Name);
+            Assert.IsNotNull(odList);
+        }
+
+        [TestMethod()]
+        public void FindByGoodsNameTest1()
+        {
+            Customer jack = new Customer(1, "Jack");
+            Goods pen = new Goods(1, "Pen", 150);
+            Goods pencil = new Goods(2, "Pencil", 5.99);
+            Goods notebook = new Goods(3, "Notebook", 15.59);
+            OrderDetails orderDetails1 = new OrderDetails(1, pen, 15);
+            OrderDetails orderDetails2 = new OrderDetails(2, pencil, 60);
+            OrderDetails orderDetails3 = new OrderDetails(3, notebook, 50);
+            Order order = new Order(1, jack);
+            order.AddDetails(orderDetails1);
+            order.AddDetails(orderDetails2);
+            order.AddDetails(orderDetails3);
+            OrderService os = new OrderService();
+            os.Insert(order);
+            List<Order> odList = os.FindByGoodsName("Pen");
+            Assert.IsNotNull(odList);
+        }
+
+        [TestMethod()]
+        public void RemoveTest1()
+        {
+            Customer jack = new Customer(1, "Jack");
+            Goods pen = new Goods(1, "Pen", 150);
+            Goods pencil = new Goods(2, "Pencil", 5.99);
+            Goods notebook = new Goods(3, "Notebook", 15.59);
+            OrderDetails orderDetails1 = new OrderDetails(1, pen, 15);
+            OrderDetails orderDetails2 = new OrderDetails(2, pencil, 60);
+            OrderDetails orderDetails3 = new OrderDetails(3, notebook, 50);
+            Order order = new Order(1, jack);
+            order.AddDetails(orderDetails1);
+            order.AddDetails(orderDetails2);
+            order.AddDetails(orderDetails3);
+            OrderService os = new OrderService();
+            os.Insert(order);
+            os.Remove(1);
+            Order od = os.GetById(1);
+            Assert.IsNull(od);
+        }
+    }
+}
